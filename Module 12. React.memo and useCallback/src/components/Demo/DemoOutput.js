c0a9757573f05f63c3b5c6893b900a503d22025c
GeorgeOrzanescu <65309085+GeorgeOrzanescu@users.noreply.github.com>
2021-11-22 13:06:20 +0200

    Delete Module 12. React.memo and useCallback directory

--- Module 12. React.memo and useCallback/src/components/Demo/DemoOutput.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-unused-vars */
-import React from "react";
-import MyParagraph from "./MyParagraph";
-
-const DemoOutput = (props) => {
-  return <MyParagraph>{props.show ? "This is new" : ""}</MyParagraph>;
-};
-
-export default React.memo(DemoOutput);
